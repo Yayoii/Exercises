137f94dd1a9296c1922823168cda75ea2f50f551
Yayoi Naruse <yayoi.naruse@g.densan-ginza.co.jp>
2021-04-19 17:10:18 +0900

    0419files

--- test/holiday.rb
+++ test/holiday.rb
@@ -6,20 +6,21 @@
 holiday = gets.chomp.to_i
 weekdays_holidays_array = gets.chomp.split(' ').map { |s| s.to_i }
 
-array = []
-answer_array = []
-(holiday-6).times do |i|
-  check_array = weekdays_holidays_array[i..i+6]
+array = [] # 週休2日以上で働き続けられる期間を入れる配列
+answer_array = [] # 後で最長期間を比べるための空の配列
+first_set = 6 # 最初の６文字分
+(holiday - first_set).times do |i|
+  check_array = weekdays_holidays_array[i..i + first_set] # 一週間毎（７要素毎）に調べていく
   zero_array = check_array.select { |n| n == 0 }
   if zero_array.size >= 2
     next check_array.map { |n| array << n } if array.empty?
-    array << check_array.pop
+    array << check_array[-1]
   elsif zero_array.size < 2 && !array.empty?
     answer_array << array
     array.clear
   end
+  answer_array << array if i == (holiday - first_set - 1 ) # 最後にチェックした配列をanswer_arrayに入れる
 end
-answer_array << array
 
 answer = []
 answer_array.size.times do |i|
